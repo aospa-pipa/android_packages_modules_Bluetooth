4549233dcebbdbd92662ff3c2c0d757df3373fdc
penghua <quic_penghua@quicinc.com>
2025-01-14 14:47:27 +0800

    Check if sdp_conn_timer is null before it is used
    Make sure sdp_conn_timer is not null before it is used.
    
    Change-Id: I7ee4d4b85a08325ff75b1dbfd1ce7799acf26661
    CRs-Fixed: 4024811

--- system/stack/sdp/sdp_main.cc
+++ system/stack/sdp/sdp_main.cc
@@ -175,7 +175,8 @@ static void sdp_config_cfm(uint16_t l2cap_cid, uint16_t /* initiator */, tL2CAP_
     sdp_disc_connected(p_ccb);
   } else {
     /* Start inactivity timer */
-    alarm_set_on_mloop(p_ccb->sdp_conn_timer, SDP_INACT_TIMEOUT_MS, sdp_conn_timer_timeout, p_ccb);
+    if (p_ccb->sdp_conn_timer)
+        alarm_set_on_mloop(p_ccb->sdp_conn_timer, SDP_INACT_TIMEOUT_MS, sdp_conn_timer_timeout, p_ccb);
   }
 }
 
