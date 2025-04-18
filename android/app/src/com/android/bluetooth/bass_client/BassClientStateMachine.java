2612cc7f3f801269db3277f0e63e04b9dcc14f15
xiaowang <quic_xiaowang@quicinc.com>
2025-04-18 17:03:27 +0800

    Bass: Fix NullPointerException in channel/subgroup building
    CRs-Fixed: 4119082
    Change-Id: I77e3456e71c1bc72a72a4fca95a8d66583f1f72e

--- android/app/src/com/android/bluetooth/bass_client/BassClientStateMachine.java
+++ android/app/src/com/android/bluetooth/bass_client/BassClientStateMachine.java
@@ -33,6 +33,7 @@ import android.bluetooth.BluetoothGattCallback;
 import android.bluetooth.BluetoothGattCharacteristic;
 import android.bluetooth.BluetoothGattDescriptor;
 import android.bluetooth.BluetoothGattService;
+import android.bluetooth.BluetoothLeAudioCodecConfigMetadata;
 import android.bluetooth.BluetoothLeAudioContentMetadata;
 import android.bluetooth.BluetoothLeBroadcastAssistant;
 import android.bluetooth.BluetoothLeBroadcastChannel;
@@ -435,7 +436,11 @@ class BassClientStateMachine extends StateMachine {
             BluetoothLeBroadcastChannel.Builder channel =
                     new BluetoothLeBroadcastChannel.Builder();
             channel.setChannelIndex(0);
+            channel.setCodecMetadata(
+                    BluetoothLeAudioCodecConfigMetadata.fromRawBytes(new byte[0]));
             subGroup.addChannel(channel.build());
+            subGroup.setCodecSpecificConfig(
+                    BluetoothLeAudioCodecConfigMetadata.fromRawBytes(new byte[0]));
             subGroup.setContentMetadata(subgroupMetadata.get(i));
             metaData.addSubgroup(subGroup.build());
         }
