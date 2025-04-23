e8f07aafbdc34bff02d2bd379a16a7493613fbec
Ritvik Mahesh <quic_rmahesh@quicinc.com>
2025-04-23 14:49:37 +0530

    RAS: Increase timer to 2 seconds to receive RAS packets when running procedure in low power mode.
    Has the following changes,
    1. Add change to update connection parameters in dev record, every time we receive a le connection update event.
    2. Increase timer to 2 seconds to receive every RAS packets when running procedure in low power mode (higher connection interval).
    
    CRs-Fixed: 4086817
    
    Change-Id: Id9d2941eb87b81bb112058c3ad45b87f24a711dc

--- system/stack/acl/ble_acl.cc
+++ system/stack/acl/ble_acl.cc
@@ -144,7 +144,12 @@ void acl_ble_update_event_received(tHCI_STATUS status, uint16_t handle, uint16_t
 
   tBTM_SEC_DEV_REC* p_dev_rec = btm_find_dev_by_handle(handle);
 
-  if (!p_dev_rec) {
+  if (p_dev_rec) {
+    p_dev_rec->conn_params.peripheral_latency = latency;
+    p_dev_rec->conn_params.min_conn_int = interval;
+    p_dev_rec->conn_params.max_conn_int = interval;
+    p_dev_rec->conn_params.supervision_tout = timeout;
+  } else {
     return;
   }
 
