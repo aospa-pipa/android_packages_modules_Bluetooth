032339d80afb1756d8050962c2be285e8f61d0d2
wentjin <quic_wentjin@quicinc.com>
2025-04-23 16:59:31 +0800

    Set mHasFallback to false for native disconnecting/disconnected
    Issue scenario:
    1. Connected CSIP device and played music.
    2. Unpaired device during streaming.
    Result: Music didn't pause sometimes.
    
    Root cause: The hasFallbackDevice didn’t be set to false when
    native disconnected of first earbud so that BT notified MM with
    suppressNoisyIntent was true.
    
    Fixed: Set setDisconnected to false when native state is
    disconnecting or disconnected.
    
    Change-Id: I0267ff696c26adb79e8720fa317718b07fe984c1
    CRs-Fixed: 4123398

--- android/app/src/com/android/bluetooth/le_audio/LeAudioService.java
+++ android/app/src/com/android/bluetooth/le_audio/LeAudioService.java
@@ -3616,6 +3616,7 @@ public class LeAudioService extends ProfileService {
                         case LeAudioStackEvent.CONNECTION_STATE_DISCONNECTING:
                         case LeAudioStackEvent.CONNECTION_STATE_DISCONNECTED:
                             deviceDescriptor.mAclConnected = false;
+                            setDisconnected(true);
 
                             if (isScannerNeeded()) {
                                 mScanCallback.startBackgroundScan();
