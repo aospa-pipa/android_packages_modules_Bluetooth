6bc7cd2b796eb46fc9209ded260ce28816b227dd
Shubham Singhal <quic_shubsing@quicinc.com>
2025-05-05 12:10:05 +0530

    [autocherry] Call not answered sometimes after sending Accept from inactive LE device am: 8500a98e82
    Change-Id: I5befa450a831be1b3950766706ad7fc514d3ae89

--- android/app/src/com/android/bluetooth/le_audio/LeAudioService.java
+++ android/app/src/com/android/bluetooth/le_audio/LeAudioService.java
@@ -2385,10 +2385,8 @@ public class LeAudioService extends ProfileService {
                 if (deviceInfo.isSink()) {
                     mAudioManagerAddedOutDevice = device;
                 }
-                if (handleAudioDeviceAdded(
-                        device, deviceInfo.getType(), deviceInfo.isSink(), deviceInfo.isSource())) {
-                    return;
-                }
+                handleAudioDeviceAdded(device, deviceInfo.getType(),
+                                            deviceInfo.isSink(), deviceInfo.isSource());
             }
         }
 
