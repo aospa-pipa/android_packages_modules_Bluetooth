e746c4f7313e943f4e5e52b115429ec035cc88b6
penghua <quic_penghua@quicinc.com>
2025-05-09 16:59:51 +0800

    [autocherry] Check incall status before update playing state am: 8d5069da95
    Change-Id: I1e35fc7eb172415df7cb856149ecabd5f2dde5f7

--- android/app/src/com/android/bluetooth/le_audio/LeAudioService.java
+++ android/app/src/com/android/bluetooth/le_audio/LeAudioService.java
@@ -4585,6 +4585,9 @@ public class LeAudioService extends ProfileService {
         return descriptor.mInbandRingtoneEnabled;
     }
 
+    public boolean getInCall() {
+       return mInCall;
+    }
     /**
      * Set In Call state
      *
