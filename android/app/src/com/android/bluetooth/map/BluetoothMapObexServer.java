66e9dccc83dd14cb687c8c0388d7bcd2004eeb20
sravan voleti <quic_svoleti@quicinc.com>
2025-05-15 20:17:41 +0530

    [autocherry] MAP: Ignore outbox/draft folder name if current folder is same am: 7b180af3ff am: 7b180af3ff
    Change-Id: Ica3285246a27496947ad587fc18ff4fce9705f01

--- android/app/src/com/android/bluetooth/map/BluetoothMapObexServer.java
+++ android/app/src/com/android/bluetooth/map/BluetoothMapObexServer.java
@@ -31,7 +31,7 @@ import android.os.UserManager;
 import android.telephony.TelephonyManager;
 import android.text.format.DateUtils;
 import android.util.Log;
-
+import com.android.bluetooth.DeviceWorkArounds;
 import com.android.bluetooth.BluetoothMethodProxy;
 import com.android.bluetooth.BluetoothStatsLog;
 import com.android.bluetooth.SignedLongLong;
@@ -650,7 +650,19 @@ public class BluetoothMapObexServer extends ServerRequestHandler {
                                 + folderName
                                 + " resulted in this element: "
                                 + folderElement.getName());
-            }
+            } else {
+                String remoteAddress = BluetoothMapService.getBluetoothMapService().
+                        getRemoteDevice().getAddress();
+                if ((DeviceWorkArounds.addressStartsWith(remoteAddress,
+                        DeviceWorkArounds.HYUNDAI_SONATA_CARKIT)) &&
+                   folderName.equalsIgnoreCase(mCurrentFolder.getName()) &&
+                   (folderName.equalsIgnoreCase(BluetoothMapContract.FOLDER_NAME_OUTBOX) ||
+                    folderName.equalsIgnoreCase(BluetoothMapContract.FOLDER_NAME_DRAFT))) {
+                    Log.d(TAG, "Current Folder name = outbox , draft or drafts so using current "
+                                + " folder only");
+                    folderElement = mCurrentFolder;
+                }
+             }
         }
         return folderElement;
     }
