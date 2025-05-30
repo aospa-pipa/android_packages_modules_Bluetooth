654a75c993458203a947f14c954a7b0bdedc2528
Shashank Dash <quic_vendetta@quicinc.com>
2025-05-30 17:10:42 +0530

    [autocherry] A2DP: Update MM Audio when unsupported Sample Rate is chosen am: e460815e5a am: e460815e5a
    Change-Id: I090c1f4c8baed19b876e4b4f255c8491d369e95d

--- android/app/src/com/android/bluetooth/a2dp/A2dpService.java
+++ android/app/src/com/android/bluetooth/a2dp/A2dpService.java
@@ -111,6 +111,7 @@ public class A2dpService extends ProfileService {
     private final boolean mA2dpOffloadEnabled;
     private final boolean mAlsDisabled;
     private final boolean mA2dpCodecExtensiblityEnabled;
+    private boolean mSetCodecConfig;
 
     // Head tracker available
     private static final long HEAD_TRACKER_AVAILABLE_MASK = 0x00300000;
@@ -152,6 +153,7 @@ public class A2dpService extends ProfileService {
                                         ("persist.vendor.service.bt.als_disabled", false);
         mA2dpCodecExtensiblityEnabled = SystemProperties.getBoolean(
              "persist.vendor.qcom.bluetooth.a2dp_offload_codec_extensibility", false);
+        mSetCodecConfig = false;
         mMaxConnectedAudioDevices = mAdapterService.getMaxConnectedAudioDevices();
         Log.i(TAG, "Max connected audio devices set to " + mMaxConnectedAudioDevices);
 
@@ -213,6 +215,8 @@ public class A2dpService extends ProfileService {
         }
 
         mHandler.removeCallbacksAndMessages(null);
+
+        enableSetCodecConfig(false);
     }
 
     CompanionDeviceManager getCompanionDeviceManager() {
@@ -1537,5 +1541,20 @@ public class A2dpService extends ProfileService {
                     BluetoothProfileConnectionInfo.createA2dpInfo(false, -1));
             return 1;
         }
+    }
+
+    public boolean isA2dpExtensibilityEnabled() {
+        Log.e(TAG, "isA2dpExtensibilityEnabled: " + mA2dpCodecExtensiblityEnabled);
+        return mA2dpCodecExtensiblityEnabled;
+    }
+
+    public void enableSetCodecConfig(boolean isSetConfig) {
+        Log.e(TAG, "enableSetCodecConfig: " + isSetConfig);
+        mSetCodecConfig = isSetConfig;
+    }
+
+    public boolean fetchSetCodecConfig() {
+        Log.e(TAG, "fetchSetCodecConfig: " + mSetCodecConfig);
+        return mSetCodecConfig;
     }
 }
