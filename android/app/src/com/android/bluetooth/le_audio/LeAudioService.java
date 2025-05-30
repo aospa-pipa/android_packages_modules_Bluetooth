298e58cd1e8e975ba3b2d6bbf25fb1d215a85f23
wentjin <quic_wentjin@quicinc.com>
2025-05-30 11:30:00 +0800

    [autocherry] Correct mHasFallback when in-active device gets disconnected am: 49f78896a5 am: 49f78896a5
    Change-Id: I7f20ac7b25a4325bbad0718f3d5fdf49314f8f57

--- android/app/src/com/android/bluetooth/le_audio/LeAudioService.java
+++ android/app/src/com/android/bluetooth/le_audio/LeAudioService.java
@@ -1032,7 +1032,6 @@ public class LeAudioService extends ConnectableProfile {
             return false;
         }
 
-        setDisconnected(true);
         sm.sendMessage(LeAudioStateMachine.DISCONNECT);
 
         return true;
@@ -3656,7 +3655,13 @@ public class LeAudioService extends ConnectableProfile {
                         case LeAudioStackEvent.CONNECTION_STATE_DISCONNECTING,
                                 LeAudioStackEvent.CONNECTION_STATE_DISCONNECTED -> {
                             deviceDescriptor.mAclConnected = false;
-                            setDisconnected(true);
+                            if (descriptor.isActive()) {
+                                if (getConnectedPeerDevices(groupId).size() > 1) {
+                                    Log.d(TAG, "There are other connected group members.");
+                                } else {
+                                    setDisconnected(true);
+                                }
+                            }
 
                             if (isScannerNeeded()) {
                                 mScanCallback.startBackgroundScan();
@@ -4551,6 +4556,8 @@ public class LeAudioService extends ConnectableProfile {
                             false,
                             hasFallbackDevice,
                             false);
+                    Log.d(TAG, "Device updated had been done, reset mHasFallback");
+                    mHasFallback = true;
                     /* Set by default earliest connected device */
                     if (Flags.leaudioBroadcastPrimaryGroupSelection()
                             && mUnicastGroupIdDeactivatedForBroadcastTransition == groupId) {
