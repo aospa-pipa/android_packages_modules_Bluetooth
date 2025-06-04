f3c0301d452ba447cf4d6f9d474ffbadbff6c140
Pragati <quic_pragati@quicinc.com>
2025-06-04 14:55:05 +0530

    [autocherry] BCS : Adding PTS flag for enabling indications for Ranging Data Ready am: 4d65679fb9 am: 4d65679fb9
    Change-Id: Ice564d50040d7d8c7c47681fa513dc41cd4a9bb6

--- system/internal_include/stack_config.h
+++ system/internal_include/stack_config.h
@@ -46,6 +46,7 @@ typedef struct {
   bool (*get_pts_rfcomm_rls_check)(void);
   bool (*get_pts_bcs_rej_write_req)(void);
   bool (*get_pts_bcs_ranging_select)(void);
+  bool (*get_enable_ind_ras_real_time)(void);
   int (*get_pts_l2cap_ecoc_send_num_of_sdu)(void);
   bool (*get_pts_l2cap_ecoc_reconfigure)(void);
   const std::string* (*get_pts_broadcast_audio_config_options)(void);
