9ef4e112af59740ee74163781b9a48d2b0b5e1a3
Ritvik Mahesh <quic_rmahesh@quicinc.com>
2025-07-07 14:13:00 +0530

    [autocherry] BT: Allow UUIDs update for Bluetooth in turning on state. am: 9a008f502a am: 9a008f502a
    Change-Id: I334d10654e3dea57420b559c5b0003fbdc9a5299

--- framework/java/android/bluetooth/BluetoothDevice.java
+++ framework/java/android/bluetooth/BluetoothDevice.java
@@ -2769,7 +2769,8 @@ public final class BluetoothDevice implements Parcelable, Attributable {
     @RequiresPermission(BLUETOOTH_CONNECT)
     public ParcelUuid[] getUuids() {
         final IBluetooth service = getService();
-        if (service == null || !isBluetoothEnabled()) {
+        BluetoothAdapter adapter = BluetoothAdapter.getDefaultAdapter();
+        if (service == null || (adapter != null && adapter.getState() != BluetoothAdapter.STATE_ON && adapter.getState() != BluetoothAdapter.STATE_TURNING_ON)) {
             Log.e(TAG, "BT not enabled. Cannot get remote device Uuids");
             if (DBG) log(Log.getStackTraceString(new Throwable()));
         } else {
