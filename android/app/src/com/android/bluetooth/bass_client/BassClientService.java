b3c1a5a04719fde5bc943bee07720b6b75763f01
zhenchao <quic_zhenchao@quicinc.com>
2025-08-22 15:20:18 +0800

    Revert "BassClient: Fake a scan record if not found"
    This reverts commit f37126896d4a4c03a7b222f9a5cc8e4428d23ad9.
    
    CRs-Fixed: 4257611
    Change-Id: Id7c54e1e1ce44400f2e39676982017632d2c5eb4

--- android/app/src/com/android/bluetooth/bass_client/BassClientService.java
+++ android/app/src/com/android/bluetooth/bass_client/BassClientService.java
@@ -2871,33 +2871,6 @@ public class BassClientService extends ConnectableProfile {
 
         ScanResult scanRes = getCachedBroadcast(broadcastId);
         if (scanRes == null) {
-            synchronized (mPendingSourcesToAdd) {
-                for (AddSourceData pendingSourcesToAdd : mPendingSourcesToAdd)
-                {
-                    if (pendingSourcesToAdd.sourceMetadata.getBroadcastId() == broadcastId) {
-                        BluetoothLeBroadcastMetadata sourceMetadata =
-                                pendingSourcesToAdd.sourceMetadata;
-                        Log.d(TAG, "Cannot find scan result, fake a scan result for QR scan case");
-                        int sid = sourceMetadata.getSourceAdvertisingSid();
-                        if (sid == -1) {
-                            sid = 0; // advertising set id 0 by default
-                        }
-                        BluetoothDevice source = sourceMetadata.getSourceDevice();
-                        int addressType = sourceMetadata.getSourceAddressType();
-                        int bId = sourceMetadata.getBroadcastId();
-                        byte[] advData = {6, 0x16, 0x52, 0x18, (byte)(bId & 0xFF),
-                                (byte)((bId >> 8) & 0xFF), (byte)((bId >> 16) & 0xFF)};
-                        ScanRecord record = ScanRecord.parseFromBytes(advData);
-                        scanRes = new ScanResult(source, addressType, 0x1 /* eventType */,
-                                0x1 /* primaryPhy */, 0x2 /* secondaryPhy */, sid, 0 /* txPower */,
-                                0 /* rssi */, 0 /* periodicAdvertisingInterval */, record,
-                                0 /* timestampNanos */);
-                        break;
-                    }
-                }
-            }
-        }
-        if (scanRes == null) {
             Log.d(TAG, "addSelectSourceRequest: ScanResult empty");
             return;
         }
@@ -3282,10 +3255,15 @@ public class BassClientService extends ConnectableProfile {
                                 new AddSourceData(device, sourceMetadata, isGroupOp));
                         // If the source has been synced before, try to re-sync
                         // with the source by previously cached scan result.
-                    } else {
+                    } else if (getCachedBroadcast(broadcastId) != null) {
                         mPendingSourcesToAdd.add(
                                 new AddSourceData(device, sourceMetadata, isGroupOp));
                         addSelectSourceRequest(broadcastId, /* hasPriority */ true);
+                    } else {
+                        Log.w(TAG, "AddSource: broadcast not cached, broadcastId: " + broadcastId);
+                        mCallbacks.notifySourceAddFailed(
+                                sink, sourceMetadata, BluetoothStatusCodes.ERROR_BAD_PARAMETERS);
+                        return;
                     }
                 } else {
                     Log.w(TAG, "AddSource: invalid broadcastId");
