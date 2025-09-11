d5a960da204f5e1dbe02edef095f6c6e407c2376
Sagar Verma <quic_sagarv@quicinc.com>
2025-09-11 16:41:37 +0530

    [autocherry] BT: Invoke SoC Add On Feature Commands Unconditionally am: b03de51854
    Change-Id: Ic858a7af73bd875746877d08eb54c63c99fd4c43

--- system/bta/dm/bta_dm_act.cc
+++ system/bta/dm/bta_dm_act.cc
@@ -296,9 +296,7 @@ void BTA_dm_on_hw_on() {
   get_btm_client_interface().link_policy.BTM_WritePageTimeout(
           osi_property_get_int32(PROPERTY_PAGE_TIMEOUT, p_bta_dm_cfg->page_timeout));
 
-  if (!osi_property_get_bool("persist.vendor.qcom.bluetooth.vsc_enabled", false)) {
-    get_btm_client_interface().vendor.BTM_ReadVendorAddOnFeatures();
-  }
+  get_btm_client_interface().vendor.BTM_ReadVendorAddOnFeatures();
 
   if (ble_vnd_is_included()) {
     get_btm_client_interface().ble.BTM_BleReadControllerFeatures(
