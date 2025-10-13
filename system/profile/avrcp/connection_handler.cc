cba98118a46b481b2f2dda5cd9971b96e3df8218
xiaowang <quic_xiaowang@quicinc.com>
2025-10-13 17:45:51 +0800

    AVRCP: Fix eventfd leakage issue
    Bluetooth crashed due to too much fd descriptors opened.
    
    Use case:
    0. Run a BLE test app to make sure entering BLE-ON state
       when turn off BT
    1. Launch BT stress test apk
    2. Select toggle Bluetooth
    3. Set the number of iterations, eg 10000 times,
       which take about 6 hours
    4. Tap Start test to begin the bluetooth on/off stress test
    5. After the apk finishes runing. check whether the bluetooth
       serivce has crashed.
    
    Root cause:
    When Bluetooth is turned on, ConnectionHandler::Initialize is
    invoked to start the AVRCP acceptor connection.
    This triggers a sequence of calls — AVRC_Open, alarm_new,
    fixed_queue_new, and semaphore_new which create an eventfd descriptor.
    When Bluetooth is turned off, AVRC_Close is not called,
    so the eventfd remains open.
    Repeatedly turning Bluetooth on and off many times eventually
    exhausts the available file descriptors, leading to a crash.
    
    Fix:
    Ensure that AVRC_Close is invoked to close all file descriptors
    when the BT is turn off
    
    CRs-Fixed: 4317262
    Change-Id: Ibe34b50d61b782833c7abb5d1ed2c71733c09073

--- system/profile/avrcp/connection_handler.cc
+++ system/profile/avrcp/connection_handler.cc
@@ -49,6 +49,7 @@ namespace bluetooth {
 namespace avrcp {
 
 ConnectionHandler* ConnectionHandler::instance_ = nullptr;
+int8_t acceptor_arcp_handle = -1;
 
 // ConnectionHandler::CleanUp take the lock and calls
 // ConnectionHandler::AcceptorControlCB with AVRC_CLOSE_IND_EVT
@@ -108,11 +109,13 @@ bool ConnectionHandler::Initialize(const ConnectionCallback& callback, AvrcpInte
   instance_->sdp_ = sdp;
   instance_->vol_ = vol;
 
+  acceptor_arcp_handle = -1;
   // Set up the AVRCP acceptor connection
-  if (!instance_->AvrcpConnect(false, RawAddress::kAny)) {
+  if (!instance_->AvrcpConnect(false, RawAddress::kAny, (uint8_t*) &acceptor_arcp_handle)) {
     instance_->CleanUp();
     return false;
   }
+  log::info("{}: acceptor_arcp_handle = {}", __func__, acceptor_arcp_handle);
 
   return true;
 }
@@ -127,6 +130,13 @@ bool ConnectionHandler::CleanUp() {
     entry++;
     curr->second->DeviceDisconnected();
     instance_->avrc_->Close(curr->first);
+    if (curr->first == acceptor_arcp_handle) {
+      acceptor_arcp_handle = -1;
+    }
+  }
+  if (acceptor_arcp_handle != -1) {
+    log::info("{}: clear avrcp handle {}", __func__, acceptor_arcp_handle);
+    instance_->avrc_->Close((uint8_t) acceptor_arcp_handle);
   }
   instance_->device_map_.clear();
   instance_->feature_map_.clear();
@@ -256,6 +266,11 @@ bool ConnectionHandler::SdpLookup(const RawAddress& bdaddr, SdpCallback cb, bool
 }
 
 bool ConnectionHandler::AvrcpConnect(bool initiator, const RawAddress& bdaddr) {
+  uint8_t handle = 0;
+  return ConnectionHandler::AvrcpConnect(initiator, bdaddr, &handle);
+}
+
+bool ConnectionHandler::AvrcpConnect(bool initiator, const RawAddress& bdaddr, uint8_t* handle) {
   log::info("Connect to device {}", bdaddr);
 
   tAVRC_CONN_CB open_cb;
@@ -273,9 +288,8 @@ bool ConnectionHandler::AvrcpConnect(bool initiator, const RawAddress& bdaddr) {
   // AVRC_API requires it though.
   open_cb.control = BTA_AV_FEAT_RCTG | BTA_AV_FEAT_RCCT | BTA_AV_FEAT_METADATA | AVRC_CT_PASSIVE;
 
-  uint8_t handle = 0;
-  uint16_t status = avrc_->Open(&handle, &open_cb, bdaddr);
-  log::info("handle=0x{:x} status=0x{:x}", handle, status);
+  uint16_t status = avrc_->Open(handle, &open_cb, bdaddr);
+  log::info("handle=0x{:x} status=0x{:x}", *handle, status);
   return status == AVRC_SUCCESS;
 }
 
