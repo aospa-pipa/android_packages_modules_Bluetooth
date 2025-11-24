2e41b40eb651bf6c3da59346cdf76ea71b954022
Harshit Gupta <harsgupt@qti.qualcomm.com>
2025-11-24 12:38:32 +0530

    [BCS] Fix hardcoded ACI causing failures on single-antenna devices
    Replaced static ACI value with dynamic calculation from cs_tone_antenna_config_mapping_table_, ensuring selection reflects actual antenna capabilities of local and remote devices.
    
    CRs-Fixed: 4360837
    Change-Id: I4ba3d1817f6af4b4e96f154ea5d9c44ae059dc64

--- system/gd/hci/distance_measurement_manager_impl.cc
+++ system/gd/hci/distance_measurement_manager_impl.cc
@@ -1219,7 +1219,7 @@ struct DistanceMeasurementManagerImpl::impl : bluetooth::hal::RangingHalCallback
             min_subevent_len,
 	    max_subevent_len,
            // kToneAntennaConfigSelection,
-	    procedure_setting.tone_ant_cfg_selection,
+	    tone_antenna_config_selection,
             (CsPhy)procedure_setting.phy,
             procedure_setting.tx_pwr_delta,
             preferred_peer_antenna,
