327eff79d5bc0882eadc4ff525f4ac6b14e3c2e3
Harshit Gupta <harsgupt@qti.qualcomm.com>
2025-11-25 16:15:55 +0530

    [BCS]: Fix security enable logic for LE roles
    - Fixes an issue where the LE_CS_SECURITY_ENABLE command was not sent by an LE Central that was also a CS Reflector.
    - The logic now correctly checks the device's LE role (Central/Peripheral) instead of its CS role (Initiator/Reflector).
    
    CRs-Fixed: 4362783
    Change-Id: I7465060575c5062076609bdb3154eab221808ee8

--- system/gd/hci/distance_measurement_manager_impl.cc
+++ system/gd/hci/distance_measurement_manager_impl.cc
@@ -1629,7 +1629,7 @@ struct DistanceMeasurementManagerImpl::impl : bluetooth::hal::RangingHalCallback
 
 
 
-    if (live_tracker->local_start == true) {
+    if (live_tracker->local_hci_role == hci::Role::CENTRAL) {
       // send the cmd from the BLE central only.
       send_le_cs_security_enable(connection_handle, live_tracker->local_start);
     } else {
