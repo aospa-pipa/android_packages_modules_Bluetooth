dffb4e7e38cdeb112db67355f383d5e19caeb718
Jakub Rotkiewicz <rotkiewicz@google.com>
2026-03-28 17:00:48 +0000

    offload: add connection proximity threshold to VND caps
    Add missing paramter connection_proximity_threshold_support to
    LeGetVendorCapabilities.
    
    Bug: 497047974
    Flag: EXEMPT BUGFIX
    Test: atest BluetoothHciVendorSpecificCallbackTest
    Change-Id: I670193f4afb52bd6660c2b6a21c26900284c4f92

--- offload/a2dp/hci/tests.rs
+++ offload/a2dp/hci/tests.rs
@@ -190,6 +190,7 @@ fn test_vendor_capabilities_override() {
         big_set_channel_map_classification_support: None,
         vendor_connection_handle_min: None,
         vendor_connection_handle_max: None,
+        connection_proximity_threshold_support: None,
     };
 
     let event = hci::CommandComplete {
