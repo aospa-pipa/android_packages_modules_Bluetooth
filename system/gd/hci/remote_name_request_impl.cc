92c8bbbb1c58d67f1c819fbe5c613e5bac11050e
penghua <penghua@qti.qualcomm.com>
2026-04-13 22:45:23 -0700

    Ignore RNR cancel status if the status is INVALID_COMMAND_PARAMETER
    If the Controller returns an HCI_Command_Complete event for
    HCI_Remote_Name_Request_Cancel with the error code  Invalid
    HCI Command Parameters, it means there is no a preceding
    HCI_Remote_Name_Request command. So, RNR cancel status
    is ignored.
    
    CRs-Fixed: 4022684
    Change-Id: I52c7a61f8fd2f8c4aeb678cb39c90927ca84d532

--- system/gd/hci/remote_name_request_impl.cc
+++ system/gd/hci/remote_name_request_impl.cc
@@ -311,7 +311,8 @@ void RemoteNameRequestModuleImpl::check_cancel_status(Address remote,
     return;
   }
   auto status = packet.GetStatus();
-  if (status != ErrorCode::SUCCESS) {
+  if ((status != ErrorCode::SUCCESS)
+     && (status != ErrorCode::INVALID_HCI_COMMAND_PARAMETERS)) {
     completed(status, std::array<uint8_t, 248>{}, packet.GetBdAddr());
   }
 }
